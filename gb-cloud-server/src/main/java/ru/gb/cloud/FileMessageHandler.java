192a3a41af8057979b08cfbe6859e2b43583e690
danfilof <danila.anfilofyev@gmail.com>
2022-04-20 19:33:06 +0200

    Generally homework completed:
    - Client has access to local package (String names of files);
    - Client can add his/her files(String names of files) into the server package.

--- gb-cloud-server/src/main/java/ru/gb/cloud/FileMessageHandler.java
+++ gb-cloud-server/src/main/java/ru/gb/cloud/FileMessageHandler.java
@@ -1,16 +1,21 @@
 package ru.gb.cloud;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class FileMessageHandler implements Runnable{
 
     private final File dir;
     private final DataInputStream is;
     private final DataOutputStream os;
+
+
+
 
     public FileMessageHandler(Socket socket) throws IOException {
         is = new DataInputStream(socket.getInputStream());
@@ -30,12 +35,30 @@ public class FileMessageHandler implements Runnable{
             while (true) {
                 String received = is.readUTF();
                 System.out.println(received);
-                if (received.equals("#AddFile#")) {
-
+                if (received.equals("#addFile#")) {
+                    String fileName = is.readUTF();
+                    System.out.println("FileName: " + fileName);
+                    addFile(fileName);
+                    String[] files = dir.list();
+                    os.writeUTF("#list#");
+                    os.writeLong(files.length);
+                    for (String file : files) {
+                        os.writeUTF(file);
+                    }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void addFile(String fileName) throws IOException {
+        try {
+            Path path = Paths.get("ServerFiles");
+            Path newFile = Files.createFile(path.resolve(fileName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
