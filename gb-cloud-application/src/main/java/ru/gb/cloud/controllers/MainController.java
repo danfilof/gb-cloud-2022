17a2c05ca844f48991861fae75ea4b3ae15fd46a
danfilof <danila.anfilofyev@gmail.com>
2022-05-20 23:10:32 +0200

    Added ability to change listviews rows color with css

--- gb-cloud-application/src/main/java/ru/gb/cloud/controllers/MainController.java
+++ gb-cloud-application/src/main/java/ru/gb/cloud/controllers/MainController.java
@@ -481,7 +481,7 @@ public class MainController implements Initializable {
 
     public void cancelAuthClick(ActionEvent actionEvent) {
         log.info("user closed the program by pressing cancel button...");
-        System.exit(0);
+        System.exit(22);
     }
 
     public void rename(ActionEvent actionEvent) {
